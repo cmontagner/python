4f08ced5fb716a9597f18646f0dfb58bc6982d92
Caroline Montagner <montagner@gmail.com>
2025-09-25 17:16:15 -0300

    Segundo exercício fundamentos - datatypes

---
+++ Fundamentos/2-dados.py
@@ -0,0 +1,14 @@
+#Pythonflix
+name = "Top Gun Maverick"
+launchYear = 2023
+movieGrade = 9.5
+planIncluded = False
+
+print(name)
+print(launchYear)
+
+#type identifica o datatype associado pelo versionador
+print(type(name))
+print(type(launchYear))
+print(type(movieGrade))
+print(type(planIncluded))
